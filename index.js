cba8612ec7b470cb125ee52b9e7d237e63b885af
Parth Dhavan <parthdhavan2004@gmail.com>
2024-07-18 02:29:45 +0530

    First Deployment Tutorial

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get('/api/get', (req, res) => {
+app.get('/', (req, res) => {
     res.send(
         { message: "hello world" }
     );
